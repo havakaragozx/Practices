3e196773d48bf1d6085f30ad8affd35ccecfe3ae
havakaragozx <havakaragozx@gmail.com>
2022-04-02 15:59:26 +0300

    Odev-3

--- Odev/Program.cs
+++ Odev/Program.cs
@@ -1,26 +1,29 @@
 ﻿using System;
- 
- /*Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). +
- Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
- Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.*/
+ /*Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). 
+ Sonrasında kullanıcıdan n adet kelime girmesi isteyin. 
+ Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın..*/
 namespace MyApp 
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.Write("Pozitif iki sayi giriniz :");
+            Console.Write("Pozitif bir sayi giriniz :");
             int n=Convert.ToInt32(Console.ReadLine());
-            int m=Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("{0} adet sayi giriniz :",n);
+            List<string> kelimeler=new List<string>();
+            
+            Console.WriteLine("{0} adet kelime giriniz :",n);
             for(int i=0;i<n;i++){
-                Console.WriteLine("{0} .Sayıyı giriniz : ",i+1);
-                int sayi=Convert.ToInt32(Console.ReadLine());
-                if((sayi==m) || (sayi %m ==0 )){
-                    Console.WriteLine("Tam bölünüyor "+sayi);
-                }
+                Console.Write("{0} .Kelimeyi giriniz : ",i+1);              
+                string kelime=Console.ReadLine();
+                kelimeler.Add(kelime);
+                
             }
-
+          int son=kelimeler.Count-1;
+          for(int i=son;i>=0;i--){
+              Console.WriteLine(kelimeler[i]);
+          }
+        
         }
     }
 }
