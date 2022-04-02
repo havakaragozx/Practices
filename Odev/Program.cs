957f3aef54c15957d6f1c6f16f7f1de15de3f228
havakaragozx <havakaragozx@gmail.com>
2022-04-02 15:48:07 +0300

    Odev-2

--- Odev/Program.cs
+++ Odev/Program.cs
@@ -1,36 +1,25 @@
 ﻿using System;
  
- /*Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin(n). 
- Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin.
- Kullanıcının girmiş olduğu sayılardan çift olanlar console'a yazdırın.*/
+ /*Bir konsol uygulamasında kullanıcıdan pozitif iki sayı girmesini isteyin (n, m). +
+ Sonrasında kullanıcıdan n adet pozitif sayı girmesini isteyin. 
+ Kullanıcının girmiş olduğu sayılardan m'e eşit yada tam bölünenleri console'a yazdırın.*/
 namespace MyApp 
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int sayi;
-           
-            Console.Write("Lütfen Pozitif bir sayı giriniz : ");
-            sayi=Convert.ToInt32(Console.ReadLine());
-            int[] sayilar=new int[sayi];
-            
-            for(int i=0;i<sayi;i++){
-             Console.Write((i+1)+ ".sayiyi giriniz: ");
-             sayilar[i]=Convert.ToInt32(Console.ReadLine());
-            }
-
-            for(int i=0;i<sayilar.Length;i++){
-                int cift=sayilar[i]%2;
-                if(cift==0){
-                    Console.WriteLine("Girilen Çift Sayılar :"+sayilar[i]);
-                }
-                else {
-
+            Console.Write("Pozitif iki sayi giriniz :");
+            int n=Convert.ToInt32(Console.ReadLine());
+            int m=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0} adet sayi giriniz :",n);
+            for(int i=0;i<n;i++){
+                Console.WriteLine("{0} .Sayıyı giriniz : ",i+1);
+                int sayi=Convert.ToInt32(Console.ReadLine());
+                if((sayi==m) || (sayi %m ==0 )){
+                    Console.WriteLine("Tam bölünüyor "+sayi);
                 }
             }
-
-            
 
         }
     }
