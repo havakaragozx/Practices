ccbaa8bff77880b995be8fa99a945c54a423f89e
havakaragozx <havakaragozx@gmail.com>
2022-04-02 15:04:06 +0300

    örnekler

--- Recursive_Extension_Metotlar/Program.cs
+++ Recursive_Extension_Metotlar/Program.cs
@@ -1,17 +1,71 @@
 ﻿using System;
 
-namespace MyApp 
+namespace MyApp
 {
-    internal class Program
+    public class NewBaseType
     {
-      public  static  void Main(string[] args)
+        public static void Main(string[] args)
         {
-            //Rekürsif - Öz yinelemeli fonksiyonlar
-            int result=1;
-            for(int i=1;i<5;i++){
-             result=result*3;
-             Console.WriteLine(result);
-            }
-        }
+            string ifade = "Hava Karagöz";
+            bool sonuc = ifade.CheckSpaces();
+            Console.WriteLine(sonuc);
+            if (sonuc)
+            Console.WriteLine(ifade.RemoveWhiteSpaces());
+          
+            Console.WriteLine(ifade.MakeUpperCase());
+            Console.WriteLine(ifade.MakeLowerCase());
+
+            int[] dizi = {4, 7, 2, 9, 3};
+            dizi.SortArray();
+            dizi.EkranaYazdir();
+
+            int sayi=5;
+            Console.WriteLine(sayi.IsEvenNumber());
+           
+           Console.WriteLine(ifade.GetFirstCharacter());
+                   } 
+        
     }
+
+        public static class Extension{
+          public static bool CheckSpaces(this string param){
+             return  param.Contains(" ");      
+                 }
+        
+        public static string RemoveWhiteSpaces(this string param)
+        {
+            string[] dizi=param.Split(" ");
+            return string.Join("",dizi);
+        }
+        
+        
+          public static string MakeUpperCase(this string param)
+          {
+             return  param.ToUpper();     
+          }
+          public static string MakeLowerCase(this string param)
+          {
+             return  param.ToLower();     
+          }
+          public static int[] SortArray(this int[] param)
+          {
+            Array.Sort(param);
+            return param;
+          }
+          
+          public static void EkranaYazdir(this int[] param){
+             foreach(var item in param){
+                Console.WriteLine(item);
+             }
+          }
+
+          public static bool IsEvenNumber(this int param){
+             return param%2 == 0;
+          }
+
+           public static string GetFirstCharacter(this string param){
+             return param.Substring(0,1);
+          }
+    }
+
 }
