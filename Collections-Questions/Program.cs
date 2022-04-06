f73c5b1dc7a41ea7b0756b47c05daf36d6716533
havakaragozx <havakaragozx@gmail.com>
2022-04-06 08:54:30 +0300

    commit-Odev3

--- Collections-Questions/Program.cs
+++ Collections-Questions/Program.cs
@@ -7,61 +7,26 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            /* Soru - 2: Klavyeden girilen 20 adet sayının en büyük 3 tanesi 
-            ve en küçük 3 tanesi bulan, her iki grubun kendi içerisinde ortalamalarını alan 
-            ve bu ortalamaları ve ortalama toplamlarını
-             console'a yazdıran programı yazınız. 
-             (Array sınıfını kullanarak yazınız.)*/
+            /*
+            Soru - 3: Klavyeden girilen cümle içerisindeki sesli harfleri bir dizi içerisinde saklayan
+             ve dizinin elemanlarını sıralayan programı yazınız.
+             */
 
+             Console.WriteLine("Bir Cümle yazınız: ");
+             string input=Console.ReadLine();
+             char[] sesliHarfler={'a','e','i','o','u'};
 
-            int[] sayilar = new int[20];
-            int[] minSayilar = new int[3];
-            int[] maxSayilar = new int[3];
-
-            Console.WriteLine("20 adet pozitif sayı giriniz: ");
-
-            for (int i = 0; i < 20; i++)
-            {
-                Console.Write("{0}.pozitif sayı :", i+1);
-                sayilar[i] = int.Parse(Console.ReadLine());
-
-            }
-            Array.Sort(sayilar);
-            double minSayac = 0;
-
-            for (int i = 0; i < 3; i++)
-            {
-                minSayilar[i] = sayilar[i];
-            }
-            Console.WriteLine("Dizinin en küçük elemanları: ");
-            foreach (var item in minSayilar)
-            {
-                Console.WriteLine(item);
-                minSayac += item;
-            }
-
-            Array.Reverse(sayilar);
-            double maxSayac = 0;
-            for (int i = 0; i < 3; i++)
-            {
-                maxSayilar[i] = sayilar[i];
-            }
-            Console.WriteLine("Dizinin en büyük elemanları: ");
-            foreach (var item in maxSayilar)
-            {
-                Console.WriteLine(item);
-                maxSayac += item;
-            }
-
-            double minAvg = minSayac / 3;
-            double maxAvg = maxSayac / 3;
-
-            Console.WriteLine("Küçük dizinin ortalamasi : " + minAvg);
-            Console.WriteLine("Büyük dizinin ortalamasi : " + maxAvg);
-            Console.WriteLine("Büyük ve Küçük dizinin ortalamaları toplamı: " + (minAvg + maxAvg));
-
-
-
+             List<char> harfler=new List<char>();
+             foreach(var item in input){
+                 foreach(var sabit in sesliHarfler){
+                     if(item==sabit)
+                     harfler.Add(item);
+                 }
+             }
+             Console.WriteLine("Cümle içerisindeki sesli harfler : ");
+             foreach(var item in harfler){
+                 Console.Write(item+" ");
+             }
         }
     }
 
