d6c534d2d5108ac746be7ce1caf887a0b7cae61c
havakaragozx <havakaragozx@gmail.com>
2022-04-08 07:25:13 +0300

    Struct

---
+++ Enum/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Gunler.Pazar);
+            Console.WriteLine((int)Gunler.Cumartesi);
+
+            int sicaklik = 23;
+            if (sicaklik <= (int)HavaDurumu.Normal)
+                Console.WriteLine("Dışarıya çıkmak için havanın ısınmasını bekleyelım.");
+
+            else if (sicaklik >= (int)HavaDurumu.Sicak)
+                Console.WriteLine("Dışarıya çıkmak için cok sıcak bir gün ");
+
+            else if (sicaklik >= (int)HavaDurumu.Normal && sicaklik < (int)HavaDurumu.CokSicak)
+                Console.WriteLine("Haydi dışarıya çıkalım.");
+        }
+    }
+    enum Gunler
+    {
+        Pazartesi,
+        Salı,
+        Çarşamba,
+        Perşembe,
+        Cuma,
+        Cumartesi,
+        Pazar
+    }
+    enum HavaDurumu
+    {
+        Soguk = 5,
+        Normal = 20,
+        Sicak = 25,
+        CokSicak = 30
+    }
+}
