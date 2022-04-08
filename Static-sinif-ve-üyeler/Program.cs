c5b3562ac8673a845c5a6f377b11a9789d8d545b
havakaragozx <havakaragozx@gmail.com>
2022-04-08 06:40:56 +0300

    static

---
+++ Static-sinif-ve-üyeler/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+
+            Console.WriteLine("Çalışan Sayısı :{0} ", Calisan.CalisanSayisi);
+            Calisan calisan = new Calisan("Hava", "Karagöz", "Yazılım");
+             Calisan calisan2 = new Calisan("Elif", "Karagöz", "Eğitim");
+            Console.WriteLine("Çalışan Sayısı :{0} ", Calisan.CalisanSayisi);
+
+            //static elemanlara sınıfın adı ile erişiyoruz.
+            Console.WriteLine("Toplama işlemi sonucu: {0}",Islemler.Topla(100,200));
+            Console.WriteLine("Çıkarma işlemi sonucu: {0}",Islemler.Cikar(400,20));
+
+        }
+    }
+    class Calisan
+    {
+        private static int calisanSayisi;
+        public static int CalisanSayisi { get => calisanSayisi; }
+        private string Isim;
+        private string Soyisim;
+        private string Departman;
+
+        static Calisan()
+        {
+            calisanSayisi = 0;
+        }
+        public Calisan(string isim, string soyisim, string departman)
+        {
+            this.Isim = isim;
+            this.Soyisim = soyisim;
+            this.Departman = departman;
+            calisanSayisi++;
+        }
+
+    }
+    static class Islemler{
+        public static long Topla(int sayi1,int sayi2){
+            return sayi1+sayi2;
+        }
+          public static long Cikar(int sayi1,int sayi2){
+            return sayi1-sayi2;
+        }
+    }
+}
